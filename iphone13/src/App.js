82792bf7d172f62a53ffc2e445036d9f1d83276b
kimye0808 <kyh011@hanyang.ac.kr>
2023-07-23 14:58:23 +0900

    flex?

--- iphone13/src/App.js
+++ iphone13/src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Header from './Header';
 import MyComponent from './MyComponent';
 import GroupIcons from './GroupIcons';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <MyComponent />
-      <GroupIcons />
+    <div className="container">
+      <div className="item">
+        <Header />
+      </div>
+      <div className="item">
+        <MyComponent />
+      </div>
+      <div className="item">
+        <GroupIcons />
+      </div>
     </div>
   );
 };
 
 export default App;
-
