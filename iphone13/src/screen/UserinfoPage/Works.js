932678e730069008d75188795abddf6ebee9d992
Iced0368 <iced03682@gmail.com>
2023-08-09 13:59:19 +0900

    구조 변경, userinfo 조금

--- iphone13/src/screen/UserinfoPage/Works.js
+++ iphone13/src/screen/UserinfoPage/Works.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import TabWrapper from './Tab/TabWrapper.js';
-import Tab from './Tab/Tab.js';
+import TabWrapper from '../Tab/TabWrapper.js';
+import Tab from '../Tab/Tab.js';
 
 import Thumbnails from '../SwiperScroll/SwiperThumbnails.js'
 
@@ -19,28 +19,6 @@ const textStyle = {
 }
 
 const Works = (userinfo) => {
-  const tabsData = [
-    {
-      title: <div style={textStyle}><b>내 작품</b></div>,
-      content: <p>This is the content for Tab 1</p>,
-    },
-    {
-      title: <div style={textStyle}><b>내 구독</b></div>,
-      content: (
-        <div>
-          <h2>Tab 2 Content</h2>
-          <p>This is another content for Tab 2.</p>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
-        </div>
-      ),
-    },
-  ];
-
-
   return (
     <div style={worksStyle}>
       <TabWrapper>
