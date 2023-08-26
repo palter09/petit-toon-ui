58a7cab22183efcf4b36ccbe3001096b635062bd
kimye0808 <kyh011@hanyang.ac.kr>
2023-08-26 17:38:57 +0900

    feat:git 대소문자 구분 설정, code: toon 404, user 404 처리

--- iphone13/src/API/APIFetcher.js
+++ iphone13/src/API/APIFetcher.js
@@ -4,7 +4,7 @@ import { reissueToken } from "./AuthentificationAPI.js";
 export async function fetchAPIAndExecute (url, options, callback, fallback) {
   try {
     let response = await fetch(url, options);
-
+    
     if (response.status === 401){
       let reissueStatus = 0;
       await reissueToken(
@@ -23,8 +23,11 @@ export async function fetchAPIAndExecute (url, options, callback, fallback) {
         };
         response = await fetch(url, options);
       }
+    }else if(response.status === 404 || response.status === 400){//fallback으로 오류 메시지 handle
+      const errorResponseData = await response.json();
+      fallback&&fallback(errorResponseData);
+      return;
     }
-
     if (response.ok) {
       // 응답이 OK이지만 JSON 데이터가 있는지 확인
       const contentType = response.headers.get("content-type");
@@ -34,7 +37,6 @@ export async function fetchAPIAndExecute (url, options, callback, fallback) {
       } else {
         // 정상 수행 되었음에도 JSON 데이터가 없는 경우 처리:ex) deleteWebtoon
         callback && callback(response.ok);
-        return;
       }
     }
   } catch (error) {
--- iphone13/src/screen/UserinfoPage.js
+++ iphone13/src/screen/UserinfoPage.js
@@ -1,19 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import Header from "./Header/Header.js"
-import Profile from './UserinfoPage/Profile.js'
-import Works from './UserinfoPage/Works.js'
-
-import { getUserInfo } from '../API/UserAPI.js';
-
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Header from "./Header/Header.js";
+import Profile from "./UserinfoPage/Profile.js";
+import Works from "./UserinfoPage/Works.js";
+import { getUserInfo } from "../API/UserAPI.js";
+import { useParams } from "react-router-dom";
+import { Error404 } from "./ToonPage/Loading404";
 
 const UserinfoPage = () => {
   const [userinfo, setUserinfo] = useState({});
+  const [is404, setIs404] = useState(false);
   const userid = useParams().id;
+
+  //404에러 핸들러
+  const handleUndefinedUser = (errorResponseData) => {
+    console.log("error code:", errorResponseData.code);
+    console.log("error message:", errorResponseData.message);
+    if (errorResponseData && errorResponseData.code === "404") {
+      setIs404(true);
+    }
+  };
 
   useEffect(() => {
     if (userid) {
-      getUserInfo(userid, setUserinfo);
+      getUserInfo(userid, setUserinfo, handleUndefinedUser);
     }
   }, [userid]);
 
@@ -21,11 +30,17 @@ const UserinfoPage = () => {
     <div className="container">
       <div className="item">
         <Header />
-        <div className='divLineHeader' />
-        <Profile userinfo={userinfo}/>
-        <div className='divLineMid' />
-        <Works userinfo={userinfo}/>
-        <div className='divLineBottom' />
+        <div className="divLineHeader" />
+        {is404 ? (
+          <Error404 what = {'유저'} />
+        ) : (
+          <>
+            <Profile userinfo={userinfo} />
+            <div className="divLineMid" />
+            <Works userinfo={userinfo} />
+            <div className="divLineBottom" />
+          </>
+        )}
       </div>
     </div>
   );
